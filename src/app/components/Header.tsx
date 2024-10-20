e81fc83e242ca01f46eb1e6217b031d606540289
Emmy Nguyen <145530039+tastytough@users.noreply.github.com>
2024-10-19 19:51:28 -0700

    feat: add sig up and log in button for header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,6 +3,7 @@ import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -45,7 +46,30 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             <BiSearch className="text-gray-200" size={20} />
           </button>
         </div>
+
+        {/* signin and logout section */}
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div>
+              <Button
+                onClick={() => {}}
+                className="bg-transparent text-neutral-300 font-medium"
+              >
+                Sign up
+              </Button>
+            </div>
+            <div>
+              <Button
+                onClick={() => {}}
+                className="bg-yellow-500 px-6 py-2 text-gray-800"
+              >
+                Log in
+              </Button>
+            </div>
+          </>
+        </div>
       </div>
+      {children}
     </div>
   );
 };
