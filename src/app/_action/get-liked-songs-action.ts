b321f7f019d138833f5f11af65a6dcc5801010ce
Emmy Nguyen <145530039+tastytough@users.noreply.github.com>
2025-01-19 13:57:04 -0800

    feat: add get-liked-songs-action

---
+++ src/app/_action/get-liked-songs-action.ts
@@ -0,0 +1,33 @@
+"use server";
+import { songs } from "@/db/schema/songs";
+import { getServerSession } from "next-auth";
+import { db, desc, eq } from "@/db/index";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { liked } from "@/db/schema/liked";
+
+export async function getLikedSongs() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    console.log("User not found");
+    return;
+  }
+
+  try {
+    const allLikedSongs = await db
+      .select({
+        title: songs.title,
+        artist: songs.artist,
+      })
+      .from(liked)
+      .innerJoin(songs, eq(liked.songId, songs.id))
+      .where(eq(liked.userId, Number(session.user.id)))
+      .orderBy(desc(liked.createdAt));
+    if (allLikedSongs.length === 0) {
+      return [];
+    }
+    return allLikedSongs;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
