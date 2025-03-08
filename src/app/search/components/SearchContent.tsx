a34f47ea9d5921aa6785f948d9d74046ee419182
Emmy Nguyen <145530039+tastytough@users.noreply.github.com>
2025-03-07 19:04:39 -0800

    feat: add player in Search

--- src/app/search/components/SearchContent.tsx
+++ src/app/search/components/SearchContent.tsx
@@ -3,11 +3,13 @@
 import MediaItem from "@/app/components/MediaItem";
 import { Song } from "../../../../types";
 import LikeButton from "@/app/components/LikeButton";
+import useOnPlay from "@/app/hooks/useOnPlay";
 
 interface SearchContentProps {
   songs: Song[];
 }
 const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
+  const onPlay = useOnPlay(songs);
   if (songs.length === 0) {
     return (
       <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
@@ -20,7 +22,10 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
       {songs.map((song) => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
           <div className="flex-1">
-            <MediaItem onClick={() => {}} data={song} />
+            <MediaItem
+              onClick={(id: number) => onPlay(String(id))}
+              data={song}
+            />
           </div>
           <LikeButton songId={song.id.toString()} />
         </div>
